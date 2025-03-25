d813eb9a13edba327ddeb481c88361b15e7fba41
Gustavo Nascimento <gs-nascimento@outlook.com>
2025-03-25 14:14:21 -0300

    chore: springboot initial structure

---
+++ src/main/java/br/com/gsndev/mudatag/MudatagApplication.java
@@ -0,0 +1,13 @@
+package br.com.gsndev.mudatag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudatagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudatagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gsndev/mudatag/MudatagApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gsndev.mudatag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudatagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
