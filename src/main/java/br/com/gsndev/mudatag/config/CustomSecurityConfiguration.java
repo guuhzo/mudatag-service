24a350557b24c3b1a147ec04adb2ab9688fabc06
Gustavo Nascimento <gs-nascimento@outlook.com>
2025-03-27 21:49:08 -0300

    feat: add security configuration

---
+++ src/main/java/br/com/gsndev/mudatag/config/CustomSecurityConfiguration.java
@@ -0,0 +1,41 @@
+package br.com.gsndev.mudatag.config;
+
+import br.com.gsndev.mudatag.filter.ValidateJwtUserAccessFilter;
+import br.com.gsndev.mudatag.user.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class CustomSecurityConfiguration {
+    private final JwtDecoder jwtDecoder;
+    private final UserService userService;
+
+    public CustomSecurityConfiguration(JwtDecoder jwtDecoder, UserService userService) {
+        this.jwtDecoder = jwtDecoder;
+        this.userService = userService;
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                )
+                .addFilterAfter(new ValidateJwtUserAccessFilter(this.jwtDecoder, this.userService), BearerTokenAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
