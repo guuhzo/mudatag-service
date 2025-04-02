ba044f08d0a128b50bbab73227c39d5fd1814af8
Gustavo Nascimento <gs-nascimento@outlook.com>
2025-04-02 16:05:24 -0300

    refactor: change base user dto from record to class

--- src/main/java/br/com/gsndev/mudatag/user/dto/BaseUserDTO.java
+++ src/main/java/br/com/gsndev/mudatag/user/dto/BaseUserDTO.java
@@ -1,10 +1,22 @@
 package br.com.gsndev.mudatag.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
-public record BaseUserDTO(
-        UUID id,
-        String fullName,
-        boolean blocked,
-        String authId
-) {}
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseUserDTO {
+    private UUID id;
+    private String fullName;
+
+    @JsonIgnore
+    private boolean blocked;
+
+    @JsonIgnore
+    private String authId;
+}
