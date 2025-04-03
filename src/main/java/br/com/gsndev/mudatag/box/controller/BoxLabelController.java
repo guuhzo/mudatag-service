6218652351f86ae0b6895dc77ade1401042f7f76
Gustavo Nascimento <gs-nascimento@outlook.com>
2025-04-03 10:49:49 -0300

    refactor: enhance get requests from box label controller

--- src/main/java/br/com/gsndev/mudatag/box/controller/BoxLabelController.java
+++ src/main/java/br/com/gsndev/mudatag/box/controller/BoxLabelController.java
@@ -1,12 +1,20 @@
 package br.com.gsndev.mudatag.box.controller;
 
+import br.com.gsndev.mudatag.box.dto.BaseBoxLabelDTO;
+import br.com.gsndev.mudatag.box.dto.ShortBoxLabelDTO;
 import br.com.gsndev.mudatag.box.model.BoxLabelModel;
 import br.com.gsndev.mudatag.box.service.BoxLabelService;
+import br.com.gsndev.mudatag.user.dto.BaseUserDTO;
+import br.com.gsndev.mudatag.util.AuthUser;
+import br.com.gsndev.mudatag.util.PaginationResponse;
 import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
@@ -22,17 +30,31 @@ public class BoxLabelController {
     }
 
     @GetMapping("")
-    public List<BoxLabelModel> findAll() {
-        return this.boxLabelService.findAll();
+    public ResponseEntity<PaginationResponse<ShortBoxLabelDTO>> findAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        BaseUserDTO authUser = AuthUser.getAuthUser();
+        Pageable pageable = PageRequest.of(page, size);
+        Page<ShortBoxLabelDTO> pageResult = this.boxLabelService.findAll(authUser.getId(), pageable);
+
+        return ResponseEntity.ok(PaginationResponse.map(pageResult));
     }
 
     @GetMapping("/{id}")
-    public BoxLabelModel findById(@PathVariable String id) {
+    public ResponseEntity<BaseBoxLabelDTO> findById(@PathVariable String id) {
+        BaseUserDTO authUser = AuthUser.getAuthUser();
+
         try {
             UUID parsedId = UUID.fromString(id);
-            return this.boxLabelService.findById(parsedId);
+
+            BaseBoxLabelDTO box = this.boxLabelService
+                    .findById(authUser.getId(), parsedId)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Box label not found"));
+
+            return ResponseEntity.ok(box);
         } catch (IllegalArgumentException e) {
-            return null;
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Box label not found");
         }
     }
 
